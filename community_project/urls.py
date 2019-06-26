d978818f9cb6154cd01860eac45cf19313f469d2
ErikaWestfall <erika_almaden@yahoo.com>
2019-06-26 11:10:06 +0800

    commit 3

--- community_project/urls.py
+++ community_project/urls.py
@@ -19,9 +19,9 @@ from portal import views as portal_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/profile/', portal_views.profile, name='profile'),
+    path('admin/', admin.site.urls), 
     path('login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='portal/logout.html'), name='logout'),
     path('', include('home.urls')),
+    path('', include('portal.urls')),
 ]
--- portal/views.py
+++ portal/views.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 
 @login_required
 def profile(request, username=None):
-    u = request.user
     if username:
         user = User.objects.get(username=username)
     else:
         user = request.user
 
-    args = {'user': user, 'title': f'{user}', 'u': u}
+    context = {'user': user, 'title': f'{user}'}
 
-    return render(request, 'portal/profile.html', args)
+    return render(request, 'portal/profile.html', context)
