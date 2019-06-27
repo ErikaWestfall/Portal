4f29ccbc563c153df589041bce9723052445c162
ErikaWestfall <erika_almaden@yahoo.com>
2019-06-27 08:48:56 +0800

    commit 4

--- portal/urls.py
+++ portal/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile, name='user-profile'),
+    path('announcements/', views.profile, name='SoM-announcements'),
+    path('events/', views.profile, name='SoM-events'),
+    path('profile/', views.profile, name='SoM-forum'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -12,3 +12,36 @@ def profile(request, username=None):
     context = {'user': user, 'title': f'{user}'}
 
     return render(request, 'portal/profile.html', context)
+
+@login_required
+def Announcements(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+    else:
+        user = request.user
+
+    context = {'user': user, 'title': 'Announcements'}
+
+    return render(request, 'portal/Announcements.html', context)
+
+@login_required
+def Events(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+    else:
+        user = request.user
+
+    context = {'user': user, 'title': 'Events'}
+
+    return render(request, 'portal/Events.html', context)
+
+@login_required
+def Forum(request, username=None):
+    if username:
+        user = User.objects.get(username=username)
+    else:
+        user = request.user
+
+    context = {'user': user, 'title': 'Forum'}
+
+    return render(request, 'portal/Forum.html', context)
